e84e3384e0ddda0b667f43100a17eff37d283ec4
mtoktaliev <mtoktalievmegacom@gmail.com>
2023-04-17 22:11:34 +0600

    end whatsapp

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
                 <div className="hero_items">
                     <p className="hero_text">Максат Токталиев</p>
                     <h1 className="hero_title">Меня зовут Максат.<br/> Я начинающий<br/> front-end разработчик.</h1>
-                    <a href='https://web.whatsapp.com/' target='_blank' rel='noreferrer'>
+                    <a href='https://api.whatsapp.com/send?phone=996500273222' target='_blank' rel='noreferrer'>
                         <button className='hero_button'><AiOutlineWhatsApp style={{fontSize: '20px'}}/>&nbsp;написать</button>
                         </a>
                 </div>
