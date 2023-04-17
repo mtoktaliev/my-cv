1d4b4c0330316de0ac25e817721041e27a6ca5ed
mtoktaliev <mtoktalievmegacom@gmail.com>
2023-04-17 20:50:31 +0600

    wa test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Service from './components/service/Service';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App wrapper">
       <Header />
       <Hero />
       <Experience />
