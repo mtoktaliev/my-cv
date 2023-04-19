3f96d8888bfbfbe8bb577c795e0c7472884b884d
mtoktaliev <mtoktalievmegacom@gmail.com>
2023-04-19 14:00:10 +0600

    dowload button

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,6 +1,8 @@
 import image from '../../assets/images/001.png'
+import resume from '../../assets/files/resume.pdf'
 import './Hero.css'
 import { AiOutlineWhatsApp } from 'react-icons/ai'
+import { HiOutlineDownload } from 'react-icons/hi'
 
 const Hero = () => {
     return (
@@ -8,11 +10,13 @@ const Hero = () => {
             <div className="hero_container">
                 <div className="hero_items">
                     <p className="hero_text">Максат Токталиев</p>
-                    <h1 className="hero_title">Меня зовут Максат.<br/> Я начинающий<br/> front-end разработчик.</h1>
+                    <h1 className="hero_title">Меня зовут Максат.<br/> Я начинающий<br/> <span>front-end</span> разработчик.</h1>
+                    <div className='hero_buttons'>
                     <a href='https://api.whatsapp.com/send?phone=996500273222' target='_blank' rel='noreferrer'>
-                        <button className='hero_button'><AiOutlineWhatsApp style={{fontSize: '20px'}}/>&nbsp;написать</button>
-                        </a>
-                        <a href="tel:996500273222">0500273222</a>
+                        <button className='hero_wabutton'><AiOutlineWhatsApp style={{fontSize: '20px'}}/>&nbsp;написать</button>
+                    </a>
+                    <a href={resume} download='resume' className='hero_dlbutton'><HiOutlineDownload style={{fontSize: '20px'}} />&nbsp;cкачать резюме</a>
+                    </div>
                 </div>
                 <div className="hero_img">
                     <img src={image} alt="" />
